fe0777d04c840b75f9d539e3783fe6f52704290f
Aman jii <151424799+Botsthe@users.noreply.github.com>
2024-01-22 12:25:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '28284211'))
 API_HASH = environ.get('API_HASH', '6b0afc140b8534c45de9dc77b7d37415')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6266456108:AAH-g6sZTjgW6RBU4okiLksT_jTUAhmHTSE")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6726031505:AAEM2q-VJOqql7_LYBX-uRtcQMtg5b0lA2U")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -61,7 +61,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Latestmoviesupdates0')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/HowToDownloadsMovie')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello Nanbha and Nanbis ❤️')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002129434061'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002062708890'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Amanchatgroup1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
